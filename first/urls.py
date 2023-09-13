6fdaffafdbc2b0c10cfaf69642879ea0e760d667
anthonphax <51837231+anthonphax@users.noreply.github.com>
2023-09-12 21:55:00 -0300

    first.url

--- first/urls.py
+++ first/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from usipav import views
 
-app_name = "basic_app"
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
